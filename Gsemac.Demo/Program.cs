03fa81e27d9c0d8b662fc5daf3e8d76de18ac008
算神 <lishewen@msn.com>
2022-06-15 09:55:29 +0800

    添加项目文件。

---
+++ Gsemac.Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gsemac.IO.Logging;
+using Gsemac.Net;
+using Gsemac.Net.Cloudflare.FlareSolverr;
+using Gsemac.Polyfills.Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+// Fix Bug:No data is available for encoding 437. 
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+static ServiceProvider CreateServiceProvider()
+{
+
+    return new ServiceCollection()
+        .AddSingleton<ILogger, ConsoleLogger>()
+        .AddSingleton<IWebClientFactory, WebClientFactory>()
+        .AddSingleton<IFlareSolverrService, FlareSolverrService>()
+        .AddSingleton<WebRequestHandler, FlareSolverrChallengeHandler>()
+        .BuildServiceProvider();
+
+}
+
+using (ServiceProvider serviceProvider = CreateServiceProvider())
+{
+
+    IWebClientFactory webClientFactory = serviceProvider.GetRequiredService<IWebClientFactory>();
+
+    using IWebClient webClient = webClientFactory.Create();
+    Console.WriteLine(webClient.DownloadString("https://www.m884d.com/"));
+
+}
+
+Console.Read();
